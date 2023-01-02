46b1cebb42374a080af745001ed0211130790123
Ggyumalang <khg2154@naver.com>
2023-01-02 13:58:45 +0900

    SQL 수정

--- Script.sql
+++ Script.sql
@@ -1,20 +1,20 @@
 -- WIFI_INFO
 CREATE TABLE `WIFI_INFO` (
-	`xSwifiMgrNo`      VARCHAR(255) NOT NULL ,
-	`xSwifiWrdofc`     VARCHAR(255) NULL     ,
+	`xSwifiMgrNo`      VARCHAR(50) NOT NULL ,
+	`xSwifiWrdofc`     VARCHAR(50) NULL     ,
 	`xSwifiMainNm`     VARCHAR(255) NULL     ,
 	`xSwifiAdres1`     VARCHAR(255) NULL     ,
 	`xSwifiAdres2`     VARCHAR(255) NULL     ,
-	`xSwifiInstlFloor` VARCHAR(255) NULL     ,
+	`xSwifiInstlFloor` VARCHAR(50) NULL     ,
 	`xSwifiInstlTy`    VARCHAR(255) NULL     ,
 	`xSwifiInstlMby`   VARCHAR(255) NULL     ,
-	`xSwifiSvcSe`      VARCHAR(20)  NULL     ,
-	`xSwifiCmcwr`      VARCHAR(255) NULL     ,
-	`xSwifiCnstcYear`  VARCHAR(255) NULL     ,
-	`xSwifiInoutDoor`  VARCHAR(255) NULL     ,
+	`xSwifiSvcSe`      VARCHAR(50)  NULL     ,
+	`xSwifiCmcwr`      VARCHAR(50) NULL     ,
+	`xSwifiCnstcYear`  VARCHAR(20) NULL     ,
+	`xSwifiInoutDoor`  VARCHAR(20) NULL     ,
 	`xSwifiRemars3`    VARCHAR(255) NULL     ,
-	`lat`              DOUBLE(8,6)  NULL     ,
-	`lnt`              DOUBLE(8,5)  NULL     ,
+	`lat`              REAL  NULL     ,
+	`lnt`              REAL  NULL     ,
 	`work_dttm`        DATETIME     NULL     
 	, PRIMARY KEY (xSwifiMgrNo)
 );
@@ -26,8 +26,9 @@ SELECT SQL FROM sqlite_master where name = 'WIFI_INFO';
 -- LOC_INQ_H
 CREATE TABLE `LOC_INQ_H` (
 	`inqNo`   INTEGER     PRIMARY KEY autoincrement,
-	`lat`     DOUBLE(8,6) NULL ,    
-	`lnt`     DOUBLE(8,5) NULL ,    
+	`lat`     REAL NULL ,    
+	`lnt`     REAL NULL ,    
 	`inq_dtm` DATETIME    NULL     
 );
+
 
